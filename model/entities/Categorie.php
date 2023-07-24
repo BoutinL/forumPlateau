ef622ea9bd0f77292ad55db2be0fbdd84420ca02
BoutinL <boutin.loic@hotmail.com>
2023-07-24 09:30:39 +0200

    entitie Categorie

---
+++ model/entities/Categorie.php
@@ -0,0 +1,55 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class Topic extends Entity{
+
+        private $id;
+        private $typeCategorie;
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of typeCategorie
+         */ 
+        public function getTypeCategorie()
+        {
+                return $this->typeCategorie;
+        }
+
+        /**
+         * Set the value of typeCategorie
+         *
+         * @return  self
+         */ 
+        public function setTypeCategorie($typeCategorie)
+        {
+                $this->typeCategorie = $typeCategorie;
+
+                return $this;
+        }
+
+    }
