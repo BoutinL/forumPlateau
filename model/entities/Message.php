deef131cab5f147c7692b43f5224cffc222e71c6
BoutinL <boutin.loic@hotmail.com>
2023-07-24 10:08:25 +0200

    Entitie Message

---
+++ model/entities/Message.php
@@ -0,0 +1,119 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class Message extends Entity{
+
+        private $id;
+        private $texteMessage;
+        private $dateCreationMessage;
+        private $sujet_id;
+        private $utilisateur_id;
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of texteMessage
+         */ 
+        public function getTexteMessage()
+        {
+                return $this->texteMessage;
+        }
+
+        /**
+         * Set the value of texteMessage
+         *
+         * @return  self
+         */ 
+        public function setTexteMessage($texteMessage)
+        {
+                $this->texteMessage = $texteMessage;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of dateCreationMessage
+         */ 
+        public function getDateCreationMessage()
+        {
+                $formattedDate = $this->dateCreationMessage->format("d/m/Y, H:i:s");
+                return $formattedDate;
+        }
+
+        /**
+         * Set the value of dateCreationMessage
+         *
+         * @return  self
+         */ 
+        public function setDateCreationMessage($date)
+        {
+                $this->dateCreationMessage = new \DateTime($date);
+
+                return $this;
+        }
+
+        /**
+         * Get the value of sujet_id
+         */ 
+        public function getSujet_id()
+        {
+                return $this->sujet_id;
+        }
+
+        /**
+         * Set the value of sujet_id
+         *
+         * @return  self
+         */ 
+        public function setSujet_id($sujet_id)
+        {
+                $this->sujet_id = $sujet_id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of utilisateur_id
+         */ 
+        public function getUtilisateur_id()
+        {
+                return $this->utilisateur_id;
+        }
+
+        /**
+         * Set the value of utilisateur_id
+         *
+         * @return  self
+         */ 
+        public function setUtilisateur_id($utilisateur_id)
+        {
+                $this->utilisateur_id = $utilisateur_id;
+
+                return $this;
+        }
+
+    }
