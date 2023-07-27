cc28eabef967e148c489a7104b521cffb051cb21
unknown <boutin.loic@hotmail.com>
2023-07-27 08:59:59 +0200

    condition pour afficher une erreur si aucun message sous un sujet

--- view/forum/listMessages.php
+++ view/forum/listMessages.php
@@ -2,22 +2,20 @@
 
 $sujet = $result['data']['sujet'];
 $messages = $result["data"]['messages'];
-
-    
 ?>
 <h1><?=$sujet->getTitreSujet()?></h1>
 <h2>Réponses:</h2>
 
 <?php
-foreach($messages as $message)
-{?> 
-    <div class="message-container"> 
-            <p><?=$message->getUtilisateur()?></p>
-            <p><?=$message->getDateCreationMessage()?></p>
-            <p><?=$message->getTexteMessage()?></p>
-        </div>
-    <?php
-}
-
-
-  
+if (!empty($messages)) {
+    foreach($messages as $message) {?> 
+        <div class="message-container"> 
+                <p><?=$message->getUtilisateur()?></p>
+                <p><?=$message->getDateCreationMessage()?></p>
+                <p><?=$message->getTexteMessage()?></p>
+            </div>
+        <?php
+    }
+} else {
+        echo "Aucune réponse n'a été soumise.";
+        }
